7ba34e406587ec2d2b3e97a0d2d36f59c7a99550
Tobias Jacobsen <tobias.cbs@gmail.com>
2016-01-20 08:48:59 +0100

    updated comments

--- src/control/Calc.java
+++ src/control/Calc.java
@@ -10,7 +10,7 @@ public class Calc {
         a.retainAll(b);
         return a;
     }
-    
+
     public List<Integer> listDifference(ArrayList<Integer> a, ArrayList<Integer> b) {
         List<Integer> aTemp = new ArrayList(a);
         a.removeAll(b);
@@ -19,9 +19,22 @@ public class Calc {
         Collections.sort(a);
         return a;
     }
-    
+
+    /**
+     * We use our existing two methods to create a union-list.
+     *
+     * Remember that: "listIntersection"-method takes everything a & b have in
+     * common. "listDifference"-method takes everything a & b do not have in
+     * common. Then the combination of these two will result a union.
+     *
+     * @param a
+     * @param b
+     * @return
+     */
     public List<Integer> listUnion(ArrayList<Integer> a, ArrayList<Integer> b) {
+        //again, we create a copy of a & b to ensure that both methods will get the same data as input
         List<Integer> intersectionList = listIntersection(new ArrayList(a), new ArrayList(b));
+
         List<Integer> differenceList = listDifference(a, b);
         differenceList.addAll(intersectionList);
         Collections.sort(differenceList);
